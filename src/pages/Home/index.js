857045f86645c6fd450f83ac60126c550a4675f3
Tom Virgo <tomcvirgo@gmail.com>
2021-08-10 16:52:25 +0100

    add material UI form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import './style.css';
 
 const Home = () => {
+    const [username, setUsername] = useState("");
+
+    function handleSubmit(event){
+        event.preventDefault();
+        window.location.pathname = `/${username}`;
+    }
+
+    function handleChange(event){
+        let newInput = event.target.value;
+        setUsername(newInput)
+    }
+
     return (
-        <h1>Home Page</h1>
+        <>
+        <form onSubmit={handleSubmit}>
+            <TextField label='GitHub Username' onChange={handleChange} value={username} className='input'/>
+            <Button type='submit' variant='outlined' color='primary' onClick={handleSubmit} className='button'>
+                Submit
+            </Button>
+        </form>
+      </>
     )
 }
 
