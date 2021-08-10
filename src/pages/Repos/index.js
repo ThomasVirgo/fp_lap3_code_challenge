6b7cddc2717d69392828d8d01e01e1b9627065d7
Tom Virgo <tomcvirgo@gmail.com>
2021-08-10 15:59:37 +0100

    add Repos component structure

--- src/pages/Repos/index.js
+++ src/pages/Repos/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Repos = () => {
     const { username } = useParams();
+    const [repoInfo, setRepoInfo] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        //make a request to github api and get repos info
+        //set repoInfo to the data recieved
+        //set loading to false
+    }, [])
+
+    //map each repository to a card component. 
+
     return (
         <h1>Repos Page for {username}</h1>
     )
