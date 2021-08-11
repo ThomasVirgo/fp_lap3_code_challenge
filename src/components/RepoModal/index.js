fec25dc2a0e5aba1c7ab5d9a8f5d840dc5d69fc4
keki94 <quequisegarra@gmail.com>
2021-08-11 11:25:48 +0100

    RepoModal with a button with icon

--- src/components/RepoModal/index.js
+++ src/components/RepoModal/index.js
@@ -1,17 +1,26 @@
 import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const useStyles = makeStyles({
-    
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     paper: {
         width: 400,
         backgroundColor: "white",
         border: '2px solid #000',
         boxShadow: 5,
         padding: 5,
-      },
-
+    },
+    giticon:{
+        marginRight:'10px',
+    }
 });  
 
 const RepoModal = ({repo,show}) => {
@@ -22,11 +31,22 @@ const RepoModal = ({repo,show}) => {
     const handleClose = () => {      
         setOpen(false);
         show();
-      };
+    };
+
+    const transformtitle=()=>{
+        const text=repo.name.replace(/_|-/gi,' ')
+        console.log(text);
+        return text;
+    }
+
+     
     const body = (
         <div className={classes.paper}>
-          <h2 id="server-modal-title">{repo.name}</h2>
-          <p id="server-modal-description">{repo.description}</p>
+            <Typography variant="h5" component="h2">{repo.name.replaceAll(/_|-/gi,' ')}</Typography>
+            <Typography  component="p">{repo.description}</Typography>
+            <Button variant="contained" color="primary" href={repo.html_url} target='_blank'>
+                <GitHubIcon className={classes.giticon}/>  See in github 
+            </Button>
         </div>
     );
 
@@ -34,6 +54,7 @@ const RepoModal = ({repo,show}) => {
         <Modal
             open={open}
             onClose={handleClose}
+            className={classes.modal}
             aria-labelledby="simple-modal-title"
             aria-describedby="simple-modal-description"
         >
